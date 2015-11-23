6092318a46afd9ed695ceb2675d3d5692063c534
Adamantish <sonsequence@hotmail.com>
2015-11-23 12:16:30 +0000

    geocoding on save

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -4,6 +4,8 @@ class ToDo < ActiveRecord::Base
 
   geocoded_by :geocode_string, latitude: :lat, longitude: :lng
 
+  before_save :geocode
+
     def geocode_string
       "#{address}, #{destination.name}"
     end
