6cf05b4e27625e082bf13072fb20f4070a718813
Adamantish <sonsequence@hotmail.com>
2015-11-25 14:33:53 +0000

    Some features

---
+++ features/step_definitions/to_do_photos_step.rb
@@ -0,0 +1,29 @@
+Given(/^a traveller has some to dos in their bucket list$/) do
+  
+      Geocoder::Lookup::Test.add_stub(
+      "Delhi, India", [
+        {
+          'latitude'     => 28.6139391,
+          'longitude'    => 77.2090212,
+          'address'      => 'Delhi, India',
+          'country'      => 'India'
+        }
+      ]
+    )
+
+  @destination = Destination.create!(name: "India")
+  ToDo.create!(description: "Ride Elephant", address: "Delhi", destination: @destination)
+
+end
+
+Given(/^a traveller is on the homepage$/) do
+  click_link "Show photos"
+end
+
+When(/^they select the photos tab$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^they should see photos of their to do item$/) do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/to_do_photos.feature
@@ -0,0 +1,9 @@
+Feature: To Do Photos
+  
+  @javascript
+  @wip
+  Scenario: 
+    Given a traveller has some to dos in their bucket list
+      And a traveller is on the homepage
+    When they select the photos tab
+    Then they should see photos of their to do item
