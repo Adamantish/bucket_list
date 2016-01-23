b278897b7e7bd8466ae58e9c882aa6c6b9e1f206
Adamantish <sonsequence@hotmail.com>
2016-01-23 16:17:55 +0000

    Trying to get migrations ignored for production

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160119202633) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "destinations", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -53,7 +56,7 @@ ActiveRecord::Schema.define(version: 20160119202633) do
     t.string   "name"
   end
 
-  add_index "travellers", ["email"], name: "index_travellers_on_email", unique: true
-  add_index "travellers", ["reset_password_token"], name: "index_travellers_on_reset_password_token", unique: true
+  add_index "travellers", ["email"], name: "index_travellers_on_email", unique: true, using: :btree
+  add_index "travellers", ["reset_password_token"], name: "index_travellers_on_reset_password_token", unique: true, using: :btree
 
 end
