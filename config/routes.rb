72a588714bf53a448c541b9a39f3a3e3e4613671
Adamantish <sonsequence@hotmail.com>
2016-02-09 20:33:22 +0000

    Part way through creating Cable

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+  # binding.pry
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -36,4 +37,7 @@ Rails.application.routes.draw do
       end
 
     end
+
+    # binding.pry
+    mount ActionCable.server , :at => '/cable'
 end
